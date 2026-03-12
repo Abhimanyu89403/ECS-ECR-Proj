cb785300e1c68c55ea7dcff15d51679f750cbe2d
Abhimanyu Bhosikar <abhimanyubhosikar@gmail.com>
2026-03-12 17:54:09 +0530

    folders created

--- terraform files/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform{
-    backend "s3" {
-        bucket = "abhibuckettfstate89403-bcde"
-        key = "dockefile/terraform.tfstate"
-        region = "ap-south-1"
-        encrypt = true
-    }
-}
--- terraform files/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {
-    description = "AWS region of the code"
-    type = string
-}
-
-variable "name" {
-    description = "Project name"
-    type = string
-}
