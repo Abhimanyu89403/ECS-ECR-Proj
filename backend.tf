a6735c741288ac4cafe7cf9c8b665824aa4a663a
Abhimanyu Bhosikar <abhimanyubhosikar@gmail.com>
2026-03-25 11:47:40 +0530

    backend state added

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "aws" {
     bucket       = "abhibuckettfstate89403-bcde"
-    key          = "ecs-ecr/tfstate"
+    key          = "ecs-ecr/dev/tfstate"
     region       = "ap-south-1"
     encrypt      = true
     use_lockfile = true
