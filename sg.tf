90bb0ba4c609ed803da67daeeb849a17cc22b06c
Abhimanyu Bhosikar <abhimanyubhosikar@gmail.com>
2026-03-30 12:03:25 +0530

    sg modified

--- sg.tf
+++ sg.tf
@@ -7,14 +7,14 @@ resource "aws_security_group" "cart_sg" {
         from_port = 80
         to_port = 80
         protocol = "TCP"
-        security_groups = [aws_security_groups.cart_sg.id]
+        security_groups = [aws_security_group.cart_sg.id]
     }
     egress {
         description = "allow outbound traffic from ecr to pull images"
         from_port = 0
         to_port = 0
         protocol = "TCP"
-        security_groups = [aws_security_groups.cart_sg.id]
+        security_groups = [aws_security_group.cart_sg.id]
     }
     tags = {
         Name = "ecs-sg"
